e900fcb3cf06dc58497218373b373eca2841ac42
Jim Kane <fastjames@gmail.com>
2022-04-22 09:06:56 -0500

    Update CI version matrix, deps
    Update CI to test the oldest and newest supported versions of
    elixir/OTP, and update dependencies to the newest available versions.

--- mix.exs
+++ mix.exs
@@ -18,7 +18,7 @@ defmodule Combine.Mixfile do
   defp deps do
     [{:ex_doc, "~> 0.13", only: :dev, runtime: false},
      {:benchfella, "~> 0.3", only: :dev, runtime: false},
-     {:dialyxir, "~> 0.5", only: :dev, runtime: false}]
+     {:dialyxir, "~> 1.1.0", only: :dev, runtime: false}]
   end
 
   defp package do
